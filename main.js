de1fc1c58698ef2a8e69b9ced89736aee6ea3ab9
miaoye que <mq435@nyu.edu>
2024-02-29 21:47:19 -0500

    added all entries

--- main.js
+++ main.js
@@ -3,30 +3,30 @@ const numEntries = $('#entries .entry').length;
 console.log("Number of entries is: ", numEntries);
 
 const entries = document.getElementById('entries');
-$('#entries').on('wheel', function (event) {
-    const dir = event.deltaY > 0 ? 0.01 : -0.01;
+// $('#entries').on('wheel', function (event) {
+//     const dir = event.deltaY > 0 ? 0.01 : -0.01;
 
-    if (dir === -0.01) { /* moving down */
-        const firstEntry = $('.entry:first');
-        console.log("First entry: ", firstEntry.children());
-        let newEntry = $('<div>').addClass('entry');
+//     if (dir === -0.01) { /* moving down */
+//         const firstEntry = $('.entry:first');
+//         console.log("First entry: ", firstEntry.children());
+//         let newEntry = $('<div>').addClass('entry');
 
-        firstEntry.children().appendTo(newEntry);
-        firstEntry.remove();
+//         firstEntry.children().appendTo(newEntry);
+//         firstEntry.remove();
 
-        $('#entries').append(newEntry);
-    } else if (dir === 0.01) { /* moving up */
-        const lastEntry = $('.entry:last');
-        console.log("Last entry: ", lastEntry.children());
-        let newEntry = $('<div>').addClass('entry');
+//         $('#entries').append(newEntry);
+//     } else if (dir === 0.01) { /* moving up */
+//         const lastEntry = $('.entry:last');
+//         console.log("Last entry: ", lastEntry.children());
+//         let newEntry = $('<div>').addClass('entry');
 
-        lastEntry.children().appendTo(newEntry);
-        lastEntry.remove();
-        $('entries').prepend(newEntry);
-    }
+//         lastEntry.children().appendTo(newEntry);
+//         lastEntry.remove();
+//         $('entries').prepend(newEntry);
+//     }
 
-    event.preventDefault();
-});
+//     event.preventDefault();
+// });
 
 // retrieve device time and set the div to center on
 const currentTimestamp = Date.now();
@@ -38,3 +38,4 @@ const fourDigitCombinedTimeStr = combinedTimeStr.padStart(4, '0');
 const time = parseInt(fourDigitCombinedTimeStr, 10);
 
 console.log(time);
+
